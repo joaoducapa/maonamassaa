009cc044f470ba6554a6d110cc69c958da835128
joaoducapa <163293051+joaoducapa@users.noreply.github.com>
2024-09-25 09:20:22 -0300

    hshdsgu dfbhs nbkix  hg

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,11 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent  = "aparecida nutricionista"
+titulo.textContent  = "aparecida nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+for(let contador = 0; contador < pacientes.length;i ++){
+    var peso = [contador].querySelector('.info-peso').textContent;
+    var altura = [contador].querySelector('.info-altura').textContent;
+    var imc = peso / (altura * altura);
+    pacientes[cortador].querySelector('.info-imc').textContent = imc;
+
+}
