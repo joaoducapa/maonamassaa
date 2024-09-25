29a4f9a0c861356ca3fcd215bdfe657f8d7593ab
joaoducapa <163293051+joaoducapa@users.noreply.github.com>
2024-09-25 09:26:17 -0300

    1

--- js/principal.js
+++ js/principal.js
@@ -1,11 +1,11 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent  = "aparecida nutricionista";
 
-var pacientes = document.querySelectorAll(".paciente");
+var pacientes = document.querySelectorAll('.paciente');
 for(let contador = 0; contador < pacientes.length;i ++){
     var peso = [contador].querySelector('.info-peso').textContent;
     var altura = [contador].querySelector('.info-altura').textContent;
     var imc = peso / (altura * altura);
     pacientes[contador].querySelector('.info-imc').textContent = imc;
-
+    
 }
